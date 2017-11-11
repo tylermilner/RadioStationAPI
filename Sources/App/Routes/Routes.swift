872189d2950df5b8840b707ad3eddd3bb024aabd
Tyler Milner <tylermilner@gmail.com>
2017-11-10 18:31:16 -0800

    WIP getting routes defined.

--- Sources/App/Routes/Routes.swift
+++ Sources/App/Routes/Routes.swift
@@ -1,25 +1,62 @@
 import Vapor
 
+// TODO: Extract route names into a common structure to remove string literal duplication.
+
 extension Droplet {
     func setupRoutes() throws {
-        get("hello") { req in
-            var json = JSON()
-            try json.set("hello", "world")
-            return json
-        }
-
-        get("plaintext") { req in
-            return "Hello, world!"
-        }
-
-        // response to requests to /info domain
-        // with a description of the request
-        get("info") { req in
-            return req.description
-        }
-
-        get("description") { req in return req.description }
+        // JSON Example:
+//        get("hello") { req in
+//            var json = JSON()
+//            try json.set("hello", "world")
+//            return json
+//        }
         
-        try resource("posts", PostController.self)
+        // Plaintext Example:
+//        get("plaintext") { req in
+//            return "Hello, world!"
+//        }
+        
+        // Resource Controller Example:
+//        try resource("posts", PostController.self)
+        
+        get("config") { request in
+            return JSON()
+        }
+        
+        post("login") { request in
+            return JSON()
+        }
+        
+        // TODO: Create a "Controller" for nowPlaying endpoint
+        get("nowPlaying") { request in
+            return JSON()
+        }
+        
+        // TODO: Should probably be PATCH (need to update docs)
+        put("nowPlaying") { request in
+            return JSON()
+        }
+        
+        // TODO: Create a "Controller" for shows endpoint
+        get("shows") { request in
+            return JSON()
+        }
+        
+        post("shows") { request in
+            return JSON()
+        }
+        
+        // TODO: GET/PATCH/DELETE for shows/{id} endpoint (do when creating 'ShowsController')
+        
+        // TODO: Create a "Controller" for djs endpoint
+        get("djs") { request in
+            return JSON()
+        }
+        
+        post("djs") { request in
+            return JSON()
+        }
+        
+        // TODO: GET/PATCH/DELETE for djs/{id} endpoint (do when creating 'DJsController')
     }
 }
