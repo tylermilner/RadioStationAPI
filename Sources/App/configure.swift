6600081f736dde58264e5d5128db2aaf39295761
Tyler Milner <tylermilner@gmail.com>
2018-11-21 20:15:25 -0600

    Fixed compilation error due to namespace conflict with Vapor's `Config` object.

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -2,7 +2,7 @@ import FluentSQLite
 import Vapor
 
 /// Called before your application initializes.
-public func configure(_ config: inout Config, _ env: inout Environment, _ services: inout Services) throws {
+public func configure(_ config: inout Vapor.Config, _ env: inout Environment, _ services: inout Services) throws {
     /// Register providers first
     try services.register(FluentSQLiteProvider())
 
